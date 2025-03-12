10bff222b99a3152b2665d798585d227c280d9a0
MatheusTr23 <matheus.nt@uni9.edu.br>
2025-03-11 23:45:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using aula_03;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Televisao tvSala = new Televisao(22f);
+Console.WriteLine($"O tamanho da tv é: {tvSala.Tamanho}");
+
+Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+tvSala.AumentarVolume();
+
+Console.WriteLine($"O tamanho da tv é: {tvSala.Tamanho}");
+
+
+tvSala.AumentarVolume();
+Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+tvSala.AumentarVolume();
+
+//Deveria imprimir mudo
+tvSala.AlternarModoMudo();
+
+//Deveria imprimir 01
+tvSala.AumentarVolume();
+Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+
+//Deveria imprimir mudo
+tvSala.AlternarModoMudo();
+
+//Deveria imprimir volume 01
+tvSala.AlternarModoMudo();
+
+tvSala.DiminuirVolume();
+Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+
+//Deveria imprimir volume 01
+tvSala.AlternarModoMudo();
+
+
+
+
+
+
